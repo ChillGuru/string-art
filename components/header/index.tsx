06abe576de95468f7bde2eef3591f632ea448028
ChillGuru <george.tolstokory@gmail.com>
2023-07-11 19:48:43 +0500

    created header and footer components

---
+++ components/header/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+
+const Header: React.FC = () => {
+    return (
+        <header className={styles.container}>
+            КАЛЕЙДОСКОП
+        </header>
+)}
+
+export default Header;
