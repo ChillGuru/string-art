8375fe73785c520f43f4350a75bafdaf242d1cc5
ChillGuru <george.tolstokory@gmail.com>
2023-07-17 22:21:36 +0500

    Some styles for login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
-import styles from '/styles/page.module.scss'
-import { GetServerSideProps } from 'next';
+import styles from '/styles/login.module.scss'
+import LoginImage from '../public/string-art-circle.svg';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      Страница ввода кода
+    <main className={styles.main}>     
+      <h1 className={styles.header}>
+        СОЗДАЙ <br />СВОЮ КАРТИНУ НИТЯМИ <br />ИЗ ЛЮБОЙ ФОТОГРАФИИ
+      </h1>
+      <Image src={LoginImage} alt="String Art" className={styles.img}/>
+      
     </main>
   )
 }
