20b02c0c16e46140f88ab2b3d8e6a0277e2c4e36
ChillGuru <george.tolstokory@gmail.com>
2023-07-17 21:19:40 +0500

    Created header with font

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 const Header: React.FC = () => {
     return (
         <header className={styles.container}>
-            КАЛЕЙДОСКОП
+            Момент
         </header>
 )}
 
