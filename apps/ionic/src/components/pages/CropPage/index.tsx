078e6a8adb71213fb91c235fa3d097e83ce5d50c
Teelirium <angryzombi01@gmail.com>
2023-11-27 19:30:32 +0500

    add generator page

--- apps/ionic/src/components/pages/CropPage/index.tsx
+++ apps/ionic/src/components/pages/CropPage/index.tsx
@@ -1,5 +1,4 @@
 import { IonButton, useIonRouter } from '@ionic/react';
-import { useOpenCv } from 'opencv-react-ts';
 import { useRef } from 'react';
 import { Cropper, ReactCropperElement } from 'react-cropper';
 import { Redirect } from 'react-router';
@@ -12,44 +11,11 @@ import '@/styles/cropper.scss';
 import styles from './styles.module.scss';
 
 export function CropPage() {
-  const { loaded, cv } = useOpenCv();
-  const imgUrl = useAppSelector((r: RootState) => r.generator.imgUrl);
+  const imgUrl = useAppSelector((s: RootState) => s.generator.imgUrl);
   const dispatch = useAppDispatch();
   const router = useIonRouter();
 
-  const canvas = useRef<HTMLCanvasElement>(null);
   const cropper = useRef<ReactCropperElement>(null);
-  /*
-  useEffect(() => {
-    if (!canvas.current) {
-      console.log('Canvas not specified');
-      return;
-    }
-    if (!imgUrl) {
-      console.log('Image not specified');
-      return;
-    }
-    if (!cv) {
-      console.log('OpenCV not loaded yet');
-      return;
-    }
-    const IMG_SIZE = Math.min(500, canvas.current.clientWidth);
-    const ctx = canvas.current.getContext('2d')!;
-    const img = new Image();
-    img.onload = () => {
-      console.log('Image loaded!');
-      ctx.canvas.width = IMG_SIZE;
-      ctx.canvas.height = IMG_SIZE;
-      ctx.clearRect(0, 0, IMG_SIZE, IMG_SIZE);
-      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, IMG_SIZE, IMG_SIZE);
-    };
-    img.src = imgUrl;
-
-    return () => {
-      ctx.clearRect(0, 0, IMG_SIZE, IMG_SIZE);
-    };
-  }, [cv, imgUrl]);
-  */
 
   const onCrop = () => {
     if (!cropper.current) {
@@ -57,7 +23,10 @@ export function CropPage() {
       return;
     }
     cropper.current.cropper.getCroppedCanvas().toBlob((blob) => {
-      if (!blob) return console.log('Unable to create cropped img blob');
+      if (!blob) {
+        console.log('Unable to create cropped img blob');
+        return;
+      }
       dispatch(setCroppedImgUrl(blob));
       router.push('/app/generator', 'forward');
     });
@@ -68,8 +37,7 @@ export function CropPage() {
   }
 
   return (
-    <div style={{ overflowY: 'scroll' }}>
-      {loaded ? 'opencv loaded' : 'opencv loading'}
+    <div>
       <h1>Обрежьте изображение по кругу</h1>
       <Cropper
         ref={cropper}
@@ -87,7 +55,6 @@ export function CropPage() {
       <IonButton size='large' onClick={onCrop}>
         Обрезать
       </IonButton>
-      <canvas ref={canvas} />
     </div>
   );
 }
--- apps/ionic/src/components/pages/GeneratorPage/index.tsx
+++ apps/ionic/src/components/pages/GeneratorPage/index.tsx
@@ -1,12 +1,50 @@
+import { useOpenCv } from 'opencv-react-ts';
+import { useEffect, useRef } from 'react';
 import { Redirect } from 'react-router';
 
 import { useAppSelector } from '@/redux/hooks';
 import { RootState } from '@/redux/store';
 
+import styles from './styles.module.scss';
+
 export function GeneratorPage() {
+  const { loaded, cv } = useOpenCv();
+
+  const canvas = useRef<HTMLCanvasElement>(null);
+
   const croppedImgUrl = useAppSelector(
     (s: RootState) => s.generator.croppedImgUrl
   );
+
+  useEffect(() => {
+    if (!canvas.current) {
+      console.log('Canvas not specified');
+      return;
+    }
+    if (!croppedImgUrl) {
+      console.log('Image not specified');
+      return;
+    }
+    if (!cv) {
+      console.log('OpenCV not loaded yet');
+      return;
+    }
+    const IMG_SIZE = Math.min(500, canvas.current.clientWidth);
+    const ctx = canvas.current.getContext('2d')!;
+    const img = new Image();
+    img.onload = () => {
+      console.log('Image loaded!');
+      ctx.canvas.width = IMG_SIZE;
+      ctx.canvas.height = IMG_SIZE;
+      ctx.clearRect(0, 0, IMG_SIZE, IMG_SIZE);
+      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, IMG_SIZE, IMG_SIZE);
+    };
+    img.src = croppedImgUrl;
+
+    return () => {
+      ctx.clearRect(0, 0, IMG_SIZE, IMG_SIZE);
+    };
+  }, [cv, croppedImgUrl]);
 
   if (!croppedImgUrl) {
     return <Redirect to='/app/crop' />;
@@ -15,6 +53,8 @@ export function GeneratorPage() {
   return (
     <div>
       <h1>Начинаем плетение</h1>
+      {loaded ? 'opencv loaded' : 'opencv loading'}
+      <canvas ref={canvas} className={styles.imgDisplay} />
     </div>
   );
 }
