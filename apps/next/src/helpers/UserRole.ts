70e0460f9208585ae5742323527dab5fad4cd73e
stopa4ka <138002962+stopa4ka@users.noreply.github.com>
2023-12-12 16:05:04 +0500

    Delete apps/next directory

--- apps/next/src/helpers/UserRole.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod';
-
-export const userRoleSchema = z.enum(['user', 'admin']);
-
-export type UserRole = z.infer<typeof userRoleSchema>;
--- apps/next/src/pages/api/doc.ts
+++
@@ -1,5 +0,0 @@
-import { docsApiRouteHandler } from 'next-rest-framework';
-
-export default docsApiRouteHandler({
-  docsConfig: { provider: 'swagger-ui' },
-});
