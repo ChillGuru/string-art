78e1b9324df1bb241559068dd9188c29de0e935c
stopa4ka <138002962+stopa4ka@users.noreply.github.com>
2024-05-12 07:13:21 +0500

    Update models.ts

--- apps/ionic/src/modules/Auth/models.ts
+++ apps/ionic/src/modules/Auth/models.ts
@@ -1,10 +1,7 @@
 import { z } from 'zod';
 
 export const loginFormSchema = z.object({
-  code: z
-    .string()
-    .min(9, 'Код не должен быть короче 8 символов')
-    .max(9, 'Код не должен быть больше 8 символов'),
+  code: z.string().min(8, 'Код должен быть минимум 8 символов'),
 });
 
 export type TLoginForm = z.infer<typeof loginFormSchema>;
